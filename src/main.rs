6b11b21b6591755c0afdb23f30ec42f43141b1a4
TheBugFarm <its.navneet.31@gmail.com>
2026-05-08 19:16:09 +0530

    a program to find the first word of an input string

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,24 @@
+use std::io;
 fn main() {
+    let mut s = String::new();
+    println!("Enter String ");
+    io::stdin().read_line(&mut s).expect("Failed to take input");
+
+    let ans = first_word(s);
+    println!("{}", ans);
+}
+fn first_word(s: String) -> String {
+    let mut ans = String::from("");
+    for i in s.chars() {
+        if i == ' ' {
+            break;
+        }
+        ans.push(i);
+    }
+    return ans;
+}
+
+/*fn main() {
     let mut german  = String::from("du bist ein "); //create a string
     println!("{} ", german );
     german.push_str("Hund"); //Insert a String
@@ -7,4 +27,4 @@ fn main() {
     println!("{}", german);
     german.replace_range(11..german.len(), "");//to replace //Delete or Replace a String portion
     println!("{} ", german);
-}
+}*/
